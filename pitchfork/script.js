fad5047e4594a99fc6122e0eb581c80eb36cb956
Adam Pearce <1wheel@gmail.com>
2019-10-08 23:26:02 -0400

    adds pitchfork

---
+++ pitchfork/script.js
@@ -0,0 +1,31 @@
+var { _, d3, jp, fs, glob, cheerio, io, queue, request } = require('scrape-stl')
+
+
+var albums = []
+glob.sync(__dirname + '/raw/*').forEach(path => {
+  var year = _.last(path.split('/')).replace('.html', '')
+  parseHTML(fs.readFileSync(path, 'utf8')).forEach(d => {
+    d.year = year
+    albums.push(d)
+  })
+})
+
+io.writeDataSync('albums.tsv', albums)
+
+function parseHTML(html){
+  var $ = cheerio.load(html)
+  var array = []
+  $('.albumListRow').each(function(){
+    var rv = {}
+    rv.slug = $('span a', this).text()
+    rv.rank = $('.albumListRank', this).text().split('.')[0]
+    rv.date = $('.albumListDate', this).text()
+    rv.genre = $('.albumListGenre', this).text()
+    rv.artist = rv.slug.split(' - ')[0]
+    rv.album = rv.slug.split(' - ')[1]
+
+    array.push(rv)
+  })
+
+  return array
+}
