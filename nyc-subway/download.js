7396fac87963ecbed91d054af0b4450805515472
Adam Pearce <adampearce@google.com>
2020-03-21 19:08:36 -0400

    downloads files

--- nyc-subway/download.js
+++ nyc-subway/download.js
@@ -1,16 +1,46 @@
+// web.mta.info/developers/turnstile.html
+
 var { _, d3, jp, fs, glob, io, queue, request } = require('scrape-stl')
-var {scraper} = require('../nba-finals-runs/nba-util.js')
 
-var html = fs.readFileSync()
+var slugs = fs.readFileSync(__dirname + '/links.html', 'utf8')
+  .split('</a>')
+  .map(str => {
+    return str.split('.txt">')[0].split('turnstile/')[1]  
+  })
 
-slugs = _.uniq(_.flatten(games).map(d => d.Game_ID))
+
+scraper({
+  slugs,
+  slugToUrl: d => `http://web.mta.info/developers/data/nyct/turnstile/${d}.txt`,
+  slugToPath: d => __dirname + '/csv/' + d + '.csv',
+  outregex: __dirname + '/csv/*.csv',
+})
 
 
 
+function scraper({slugs, slugToPath, slugToUrl, concurancy = 1, outregex}, cb){
+  var isDownloaded = _.indexBy(glob.sync(outregex))
 
-// scraper({
-//   slugs,
-//   slugToUrl: d => `https://stats.nba.com/game/${d}/`,
-//   slugToPath: d => __dirname + '/gamedetail/' + d + '.html',
-//   outregex: __dirname + '/gamedetail/*.html',
-// })
+  var q = queue(concurancy)
+  slugs.forEach(d => q.defer(download, d))
+  q.awaitAll((err, res) => {
+    console.log(err)
+    if (cb) cb(err)
+  })
+
+  function download(slug, cb){
+    var outpath = slugToPath(slug)
+    if (isDownloaded[outpath]) return cb()
+
+    var url = slugToUrl(slug)
+    console.log(url)
+    
+    request({url}, (err, res) => {
+
+      setTimeout(cb, 2000)
+      if (err || !res.body || res.body.length < 200) return console.log(slug, err, res)
+
+      fs.writeFile(outpath, res.body, d => d)
+    })
+  }
+}
