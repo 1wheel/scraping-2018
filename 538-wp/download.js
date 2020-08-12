bd10639db2d1a4c767f2946f43e84d8e20a37c69
Adam Pearce <adampearce@google.com>
2020-08-11 22:16:06 -0400

    parses merged files

--- 538-wp/download.js
+++ 538-wp/download.js
@@ -1,14 +1,35 @@
 var { _, d3, jp, fs, glob, io, queue, request } = require('scrape-stl')
 
+var rawdir = __dirname + `/raw-data`
+
 var url = 'https://projects.fivethirtyeight.com/2020-nba-predictions/data.json'
 request({url}, (err, res, body) => {
   var dateStr = (new Date()).toISOString()
-  fs.writeFileSync(__dirname + `/raw-data/${dateStr}.json`, body)
+  fs.writeFileSync(`${rawdir}/${dateStr}.json`, body)
+
+  merge()
 })
 
 
 function merge(){
-  // TODO merge and write to public folder
+  // var lastUpdate = null
+  var out = glob.sync(rawdir + '/*.json')
+    .map((path, i) => {
+      var data = io.readDataSync(path)
+
+      // if (!lastUpdate || data.weekly_forecasts.forecasts[0].last_updated > lastUpdate.weekly_forecasts.forecasts[0].last_updated) lastUpdate = data
+
+      var rv = data.weekly_forecasts.forecasts[0]
+      rv.timestamp = path.split('/').slice(-1)[0].replace('.json', '')
+
+      return data.weekly_forecasts.forecasts[0]
+    })
+
+
+  // TODO just grab the last update from a given day
+
+  io.writeDataSync(__dirname + '/merged-forecasts.json', out)
+
 }
 
 
