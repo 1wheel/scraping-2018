7a7ad8a201b18bf3d0448e238de43cd1e2d479b9
Adam Pearce <1wheel@gmail.com>
2020-08-11 21:41:08 -0400

    keys

--- 538-wp/download.js
+++ 538-wp/download.js
@@ -1,14 +1,17 @@
 var { _, d3, jp, fs, glob, io, queue, request } = require('scrape-stl')
 
+// var url = 'https://projects.fivethirtyeight.com/2020-nba-predictions/data.json'
+// request({url}, (err, res, body) => {
+//   var data = JSON.parse(body)
+//   // if (data && data.week_forecasts)
+//   fs.writeFileSync(__dirname + '/raw-data/data.json', body)
+// })
 
-var url = 'https://projects.fivethirtyeight.com/2020-nba-predictions/data.json'
 
-request({url}, (error, response, body) => {
 
-  var data = JSON.parse(body)
 
-  // if (data && data.week_forecasts)
+var data = io.readDataSync(__dirname + '/raw-data/data.json')
 
-  console.log(data)
+console.log(Object.keys(data))
 
-})
+console.log(data.clinches)
