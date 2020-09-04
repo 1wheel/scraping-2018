3bf8a3e2925ef1fd5078924cbaa1ae85bc841149
Adam Pearce <adampearce@google.com>
2020-09-03 23:42:31 -0400

    quicker updates

--- 538-wp/update.sh
+++ 538-wp/update.sh
@@ -3,5 +3,5 @@ do
   node download.js
   cp 538-2020-nba-forecasts.json ../../../../usr/share/nginx/html/data/
   cp 538-2020-nba-games.json ../../../../usr/share/nginx/html/data/
-  sleep 4h
+  sleep 1h
 done
