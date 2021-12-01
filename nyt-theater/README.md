7dd95e6b4abcaac0b9d2a75a30883a6a0bc68cfe
Adam Pearce <adampearce@google.com>
2021-12-01 02:26:06 -0500

    theater init

---
+++ nyt-theater/README.md
@@ -0,0 +1,14 @@
+- https://www.nytimes.com/reviews/theater
+- https://www.broadwayworld.com/browseshows.cfm?showtype=BR
+- https://www.broadwayworld.com/browseshows.cfm?showtype=OF
+
+
+Staged in 2011, but reviewed in 2015?
+
+https://www.broadwayworld.com/shows/backstage.php?showid=330160
+
+https://www.nytimes.com/2015/12/09/theater/review-a-childs-christmas-in-wales-from-irish-repertory-theater.html
+
+Not listed in 2015
+
+https://www.broadwayworld.com/browseshows.cfm?showtype=OF&open_yr=2015
---
+++ nyt-theater/nyt/download.mjs
@@ -0,0 +1,60 @@
+// var {_, cheerio, d3, jp, fs, glob, io, queue, request} = require('scrape-stl')
+
+import {_, cheerio, d3, jp, fs, glob, io, queue, request} from 'scrape-stl' 
+import fetch from 'node-fetch'
+
+import { URL } from 'url'
+var __dirname = new URL('.', import.meta.url).pathname
+
+var headers = io.readDataSync(__dirname + 'headers.json')
+
+var res = await fetch("https://samizdat-graphql.nytimes.com/graphql/v2", {
+  headers,
+  "body": "{\"operationName\":\"TheaterReviewsQuery\",\"variables\":{\"first\":10,\"sortOrder\":\"ASC\",\"cursor\":\"YXJyYXljb25uZWN0aW9uOjk=\"},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"d758b4b1501b909d523c32435bbbcd265221e3cc9d945ae787c060d3086da2ea\"}}}",
+  "method": "POST"
+})
+
+var json = await res.json()
+
+var search = json.data.reviews.theater.search
+console.log(d3.keys(search))
+console.log(search.pageInfo)
+console.log(search.metadata)
+console.log(search.edges[0].node)
+
+
+// scraper({
+//   slugs,
+//   slugToUrl: d => 'https://www.nytimes.com/svc/collections/v1/publish/www.nytimes.com/by/adam-pearce?sort=newest&page=' + d,
+//   slugToPath: d => __dirname + '/raw/' + d + '.json',
+//   outregex: __dirname + '/raw/*.json',
+//   concurancy: 1,
+// })
+
+
+// function scraper({slugs, slugToPath, slugToUrl, concurancy = 1, outregex}, cb){
+//   var isDownloaded = _.indexBy(glob.sync(outregex))
+
+//   var q = queue(concurancy)
+//   slugs.forEach(d => q.defer(download, d))
+//   q.awaitAll((err, res) => {
+//     console.log(err)
+//     if (cb) cb(err)
+//   })
+
+//   function download(slug, cb){
+//     var outpath = slugToPath(slug)
+//     if (isDownloaded[outpath]) return cb()
+
+//     var url = slugToUrl(slug)
+//     console.log(slug, url)
+//     request(url, (err, res) => {
+
+//       cb()
+//       if (err || !res.body || res.body.length < 200) return console.log(slug, err, res)
+
+//       fs.writeFile(outpath, res.body, d => d)
+//     })
+//   }
+// }
+
