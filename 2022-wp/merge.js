8e3b94f4ead5a94805c42ee2a4dbd7571e29392c
Adam Pearce <adampearce@google.com>
2022-11-08 23:22:34 -0500

    combines all times

--- 2022-wp/merge.js
+++ 2022-wp/merge.js
@@ -14,38 +14,44 @@ function merge(){
       return {slug, time, path}
     })
 
-  var byTime = jp.nestBy(files, d => d.time)
-  var time = _.last(byTime)
+  var timedata = jp.nestBy(files, d => d.time).map(parseTime)
+  function parseTime(time){
+    var nyt = _.findWhere(time, {slug: 'nyt'})
+    var nytData = io.readDataSync(nyt.path)
 
-  var nyt = _.findWhere(time, {slug: 'nyt'})
-  var nytData = io.readDataSync(nyt.path)
-
-  var races = nytData.raceCollections.scoreboard_races.map(d => d.nyt_voteshare_estimate)
-  races.forEach(d => {
-    var [state, __, chamber] = d.nyt_id.split('-')
-    d.state = state
-    d.chamber = chamber
-  })
-
-  time.forEach(({slug, path}) => {
-    if (slug == 'nyt') return
-
-    var data = io.readDataSync(path)
-
-    races.forEach(race => {
-      race.time = time.key 
-      var m = data[`2022-11-08_${race.state}_G_${race.chamber}`]
-      if (m){
-        race.wapo = m.estimates
-      }
+    var races = nytData.raceCollections.scoreboard_races.map(d => d.nyt_voteshare_estimate)
+    races.forEach(d => {
+      var [state, __, chamber] = d.nyt_id.split('-')
+      d.state = state
+      d.chamber = chamber
     })
-  })
+
+    time.forEach(({slug, path}) => {
+      if (slug == 'nyt') return
+
+      var data = io.readDataSync(path)
+
+      races.forEach(race => {
+        var m = data[`2022-11-08_${race.state}_G_${race.chamber}`]
+        if (m){
+          race.wapo = m.estimates
+        }
+      })
+    })
+
+    return {races, scrapeTime: time.key}
+  }
 
   var outpath = outdir + '/2022-wp-latest.json'
-  io.writeDataSync(outpath, races)
+  io.writeDataSync(outpath, _.last(timedata))
   exec(`rsync -a ${outpath} demo@roadtolarissa.com:../../usr/share/nginx/html/data/2022-wp-latest.json`)
+
+  var outpath = outdir + '/2022-wp.json'
+  io.writeDataSync(outpath, timedata)
+  exec(`rsync -a ${outpath} demo@roadtolarissa.com:../../usr/share/nginx/html/data/2022-wp.json`)
 }
 
-// setInterval(merge, 60*1000)
+
+setInterval(merge, 60*1000)
 merge()
 
